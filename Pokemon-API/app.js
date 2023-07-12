3fed79c8cd1803ca4cf29321a80031d239678615
Paul <paumallet76@gmail.com>
2023-07-12 17:43:10 +0200

    Add routes file to implement Fastify register() API
    And also modify the original app.js file

--- Pokemon-API/app.js
+++ Pokemon-API/app.js
@@ -1,19 +1,17 @@
 import Fastify from 'fastify'
+import { routes as firstRoute } from './our-first-route.js'
 
 const fastify = Fastify({
   logger: false //see server infos
 })
 
-fastify.get('/', async (request, reply) => {
-  return { hello: 'hello world ❤️' }
-})
+// Declare a route  / import route
+fastify.register(firstRoute)  //register() : API qui permet d'add plugins, routes...
 
-/**
- * Run the server!
- */
+// Run the server!
 const start = async () => {
   try {
-    await fastify.listen({ port: 8000 })
+    await fastify.listen({ port: 8000, host: '127.0.0.1' })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
