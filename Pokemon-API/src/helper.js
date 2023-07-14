87764911e9bdd664ca3a81089b257bd3aaf3ffe6
Paul <paumallet76@gmail.com>
2023-07-14 19:48:01 +0200

    Add middlewares and API test
    Add serve-favicon Express package + POST req with Insomnia call

--- Pokemon-API/src/helper.js
+++ Pokemon-API/src/helper.js
@@ -1,7 +1,11 @@
-exports.success = (message, data) => {
+export const success = (message, data) => {
     return { message, data }
 }
 
+// CommonJS
+// exports.success = (message, data) => {
+//     return { message, data }
+// }
 // exports.success -> const success =... + exports.success
 // message -> message : message
 // la res renverra un obj qui englobera en 1er le message, puis la data du pokemon demandé
