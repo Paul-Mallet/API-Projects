60a660b26907b00f834868aa6f9a01eb32b9ae61
Paul <paumallet76@gmail.com>
2023-07-16 13:01:57 +0200

    Some tests feature before implent Mongoose

--- Pokemon-API/src/helper.js
+++ Pokemon-API/src/helper.js
@@ -1,6 +1,29 @@
-export const success = (message, data) => {
+// export const success = (message, data) => {
+//     return { message, data }
+// }
+
+// export const getUniqueId = (pokemons) => {
+//     const pokemonsIds = pokemons.map(pokemon => pokemon.id) //retourne un tab avec les id des pokemons
+//     const maxId = pokemonsIds.reduce((a,b) => Math.max(a,b))  //récup l'id le plus grand(le dernier)
+//     const uniqueId = maxId + 1  //lui ajoute 1
+//     return uniqueId
+// }
+
+
+
+exports.success = (message, data) => {
     return { message, data }
+  }
+
+exports.getUniqueId = (pokemons) => {
+    const pokemonsIds = pokemons.map(pokemon => pokemon.id)
+    const maxId = pokemonsIds.reduce((a,b) => Math.max(a, b))
+    const uniqueId = maxId + 1
+
+    return uniqueId
 }
+
+// c'est la database qui attribut un id unique, là c'est manuellement
 
 // CommonJS
 // exports.success = (message, data) => {
